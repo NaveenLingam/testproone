36594587244d6d4f78f17e32c0e08d8ac7308834
NaveenLingam <naveenlingam@10decoders.in>
2020-05-29 08:18:55 +0000

    code commited from geppetto!

---
+++ application/client/desktop/testproone/src/app/header/header.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { ITranslationService, I18NEXT_SERVICE } from 'angular-i18next';
+import { LoginService } from '../login/login.service';
+import { BroadcastService } from '../auth/broadcast.service';
+import { Router } from '@angular/router';
+
+
+declare var $: any;
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+public isAdminUser = false;
+public userId: string;
+public currentLanguage: String;
+public confirmLangChangeModal: String = 'none';
+public language = 'en';
+public languages = ['en', 'ta', 'es']
+  constructor(
+@Inject(I18NEXT_SERVICE) private i18NextService: ITranslationService,
+private router: Router,
+private loginService: LoginService,
+public broadcastService: BroadcastService
+
+) {
+	this.broadcastService.currentUserName.subscribe(headerPermission => {
+			if (headerPermission && headerPermission.Project && headerPermission.Project.Fields && headerPermission.Project.Fields.config === 'true') {
+			 this.isAdminUser = true;
+			 } else {
+				 this.isAdminUser = false;
+			 }
+	});
+}
+
+  ngOnInit() {
+    const el = document.querySelector('#nav-toggle');
+      const closeIcon = document.querySelector('#closeIcon');
+      if (closeIcon && el) {
+        closeIcon.addEventListener('click', function() {
+          document.getElementById('nav-toggle').click();
+        });
+      }
+      if (el) {
+        el.addEventListener('click', function() {
+          this.classList.toggle('active');
+          this.classList.toggle('change');
+          $('#sidebar, #content').toggleClass('active');
+          $('.collapse.in').toggleClass('in');
+          $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+        this.isAdminUser = false;
+        this.userId = sessionStorage.getItem('Id');
+      };
+  }
+
+     
+
+private updateState(lang: string) {
+		this.language = lang;
+		}
+changeLanguage(lang) {
+		if (lang !== this.i18NextService.language) {
+		this.i18NextService.changeLanguage(lang).then(x => {
+		this.updateState(lang);
+		});
+		}
+		this.userId = sessionStorage.getItem('Id');
+		if (this.userId !== null) {
+		this.logout();
+		} else {
+		document.location.reload();
+		}
+		}
+onCloseHandled() {
+		this.confirmLangChangeModal = 'none';
+		}
+confirmLangChange() {
+		this.changeLanguage(this.currentLanguage);
+		this.onCloseHandled();
+		}
+confirmLangModel(lang) {
+		this.userId= sessionStorage.getItem('Id');
+		if (this.userId !== null) {
+		this.confirmLangChangeModal = 'block';
+		this.currentLanguage = lang;
+		} else {
+		this.changeLanguage(lang);
+		this.onCloseHandled();
+		}
+		}
+ logout() {
+		const temp = {
+			 id: sessionStorage.getItem('Id')
+		};
+		this.loginService.Logout(temp).subscribe(data => {
+			sessionStorage.clear();
+		this.userId = sessionStorage.getItem('Id');
+		this.router.navigate(['']);
+		}, error => {
+			console.error('error:', error);
+		});
+		}
+}
---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://testproone-app.testproone.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://testproone-app.testproone.svc.cluster.local:3009';
+export const ADMINURL = 'http://testproone-app.testproone.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://testproone-app.testproone.svc.cluster.local:3008';
